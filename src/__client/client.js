fa59e9fd893aab692e0ce5a141db505b7b439ed6
glauroqj <glauro.juliani@hotmail.com>
2020-08-17 21:34:01 -0300

    add: some files

---
+++ src/__client/client.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { hydrate, render } from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { RestLink } from 'apollo-link-rest'
+import { BrowserRouter } from 'react-router-dom'
+// import { createHttpLink } from 'apollo-link-http'
+// import fetch from 'node-fetch'
+/** apollo */
+import { ApolloProvider } from '@apollo/react-common'
+/** layout */
+import Layout from '../routes/Layout'
+/** style */
+import { ThemeProvider } from 'styled-components'
+import { Reset } from 'styled-reset'
+import { GlobalStyle, Theme } from 'assets/style'
+/** provider */
+import { LanguageProvider } from 'providers/Language'
+
+const client = new ApolloClient({
+  link: new RestLink({
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    credentials: 'same-origin',
+    uri: `${process.env.API_URL}`,
+  }),
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+  // link: createHttpLink({
+  //   // uri: `${process.env.BASE_URL}`,
+  //   credentials: 'same-origin',
+  //   // headers: {
+  //   //   authorization: `${process.env.AUTH}`,
+  //   // },
+  //   fetch: fetch
+  // }),
+  ssrForceFetchDelay: 100
+})
+
+const rootElement = document.getElementById('root')
+
+console.log('< ROOT ELEMENT > ', rootElement, rootElement.hasChildNodes)
+
+const appTree = (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <Reset />
+        <LanguageProvider>
+          <Layout />
+        </LanguageProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </ApolloProvider>
+)
+
+if (rootElement.hasChildNodes) {
+  hydrate(appTree, rootElement)
+} else {
+  render(appTree, rootElement)
+}
---
+++ src/__server/server.js
@@ -0,0 +1,54 @@
+import Express from 'express'
+/** apollo */
+import { getDataFromTree } from '@apollo/react-ssr'
+/** core app */
+import createApolloClient from './middlewares/createApolloClient'
+/** server routes */
+import serverRoutes from './routes/serverRoutes'
+
+const app = new Express()
+
+app.use(Express.static('./build/client'))
+
+/** create apollo client */
+app.use(createApolloClient)
+
+app.use(async (req, res, next) => {
+  try {
+    await getDataFromTree(res.App)
+    next()
+  } catch(error) {
+    console.warn('< GET DATA FROM TREE : ERROR > ', error)
+    res.end()
+  }
+  // getDataFromTree(res.App)
+  // .then(() => {
+  //   /* We are ready to render for real */
+  //   const content = renderToString(res.App)
+  //   /* prepare style on server */
+  //   const sheet = new ServerStyleSheet()
+  //   const styleTags = sheet.getStyleTags()
+
+  //   const initialState = res.apolloClient.extract()
+  
+  //   const html = <Html
+  //                 styles={styleTags}
+  //                 content={content}
+  //                 state={initialState}
+  //               />
+  
+  //   res.status(200)
+  //   res.send(`<!doctype html>\n${renderToStaticMarkup(html)}`)
+  //   res.end()
+  // })
+  // .catch(error => {
+    // console.warn('< GET DATA FROM TREE : ERROR > ', error)
+    // res.end()
+  // })
+})
+
+app.use((req, res, next) => {serverRoutes(app), next()} )
+
+app.listen(process.env.PORT, process.env.HOST, () => console.log( // eslint-disable-line no-console
+  `< SERVER RUNNING : PORT ${process.env.PORT} : HOST ${process.env.HOST} >`
+))
