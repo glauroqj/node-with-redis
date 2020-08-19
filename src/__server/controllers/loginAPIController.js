94d9fb82665c2901d5b8f1115156a99afa8fb85e
glauroqj <glauro.juliani@hotmail.com>
2020-08-19 19:28:54 -0300

    add: chnage session value

--- src/__server/controllers/loginAPIController.js
+++ src/__server/controllers/loginAPIController.js
@@ -1,6 +1,19 @@
 export default async ( req, res ) => {
     console.log('< LOGIN API CONTROLLER > ', req.body)
 
+    /** call external api */
+
+    /** change req.session here */
+
+    req.session = {
+        token: 'token value',
+        userID: '123',
+        userName: 'Maneiro',
+        cookie: {
+            expires: 600000 /** 10 minutes */
+        }
+    }
+
     res.status(200)
     res.send(`OK`)
     res.end()
